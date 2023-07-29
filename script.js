d5935fe448abdcffd1aa4061efacc391804bbe10
Akezhan <akidulatini@gmail.com>
2023-07-29 22:45:20 +0600

    Add animations

---
+++ script.js
@@ -0,0 +1,51 @@
+const canvas = document.getElementById('canvas1')
+const ctx = canvas.getContext('2d')
+const CANVAS_WIDTH = canvas.width = 600
+const CANVAS_HEIGHT = canvas.height = 600
+
+const playerImage = new Image()
+playerImage.src = 'assets/shadow_dog.png'
+const spriteWidth = 575
+const spriteHeight = 523
+let playerState = 'idle'
+
+let gameFrame = 0
+const staggerFrames = 5
+const spriteAnimations = []
+const animationStates = [
+    { name: 'idle', frames: 7 },
+    { name: 'jump', frames: 7 },
+    { name: 'fall', frames: 7 },
+    { name: 'run', frames: 9 },
+    { name: 'dizzy', frames: 11 },
+    { name: 'sit', frames: 5 },
+    { name: 'roll', frames: 7 },
+    { name: 'bite', frames: 7 },
+    { name: 'ko', frames: 12 },
+    { name: 'getHit', frames: 4 },
+]
+
+animationStates.forEach((state, index) => {
+    let frames = {
+        locations: [],
+    }
+    for (let j = 0; j < state.frames; j++) {
+        let positionX = j * spriteWidth
+        let positionY = index * spriteHeight
+        frames.locations.push({ x: positionX, y: positionY })
+    }
+    spriteAnimations[state.name] = frames
+})
+
+const animate = () => {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+    let position = Math.floor(gameFrame / staggerFrames) % spriteAnimations[playerState].locations.length
+    let frameX = position * spriteWidth
+    let frameY = spriteAnimations[playerState].locations[position].y
+    ctx.drawImage(playerImage, frameX, frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
+
+    gameFrame++
+    requestAnimationFrame(animate)
+}
+
+animate()
